e39cd441829f029c4d962e2765156a8e6d57db95
Lokenath Karmakar <ilokenathkarmakar@gmail.com>
2022-04-17 13:42:15 +0000

    add validation for create-user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
+  @IsString()
   email: string;
+
+  @IsString()
+  @IsOptional()
   name?: string;
 }
