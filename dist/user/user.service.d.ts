61c125f3972e9999bea08f77f00788bd677474d2
Lokenath Karmakar <ilokenathkarmakar@gmail.com>
2022-04-17 08:49:54 +0000

    update gitpod with extensions

---
+++ dist/user/user.service.d.ts
@@ -0,0 +1,12 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+export declare class UserService {
+    private prisma;
+    constructor(prisma: PrismaService);
+    create(dto: CreateUserDto): import(".prisma/client").Prisma.Prisma__UserClient<import(".prisma/client").User>;
+    findAll(): string;
+    findOne(id: number): string;
+    update(id: number, updateUserDto: UpdateUserDto): string;
+    remove(id: number): string;
+}
