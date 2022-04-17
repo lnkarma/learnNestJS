929416fef0bd98196d003c273338781b550033c1
Lokenath Karmakar <ilokenathkarmakar@gmail.com>
2022-04-17 16:28:52 +0000

    add config module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,20 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+    console.log(config);
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
